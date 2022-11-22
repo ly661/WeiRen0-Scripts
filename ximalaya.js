601abc7ecebe619c4b83fdd7dd1ea46e76641f86
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2022-11-22 18:38:21 +0800

    Update ximalaya.js

--- ximalaya.js
+++ ximalaya.js
@@ -1,3 +1,4 @@
+/*
 #脚本功能：喜马拉雅 会员
 #下载地址：appstore
 #软件版本：所有
@@ -9,7 +10,7 @@
 #TG反馈群：https://t.me/+tOQEwAEP0B81YzVl
 #TG频道群：暂无
 #使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
-/*
+
 [rewrite_local]
 
 ^http[s]?:\/\/.+ximalaya.+(product/info|/mobile-user/v2/homePage|/vip/v1/recommand/ts|mobile-playpage/playpage/tabs|/mobile-album/album/page/ts|mobile/v1/album/track/ts|product/promotion/v./whole/album/\d+/price/dynamic/ts|business-vip-presale-mobile-web/page/ts).*$ https://raw.githubusercontent.com/WeiRen0/Scripts/main/ximalaya.js
