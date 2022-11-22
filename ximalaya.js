ca77e58ed20d9e73d290485c1525e58956446ba1
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2022-11-22 12:39:19 +0800

    Update ximalaya.js

--- ximalaya.js
+++ ximalaya.js
@@ -13,10 +13,14 @@ TG频道群：暂无
 
 [rewrite_local]
 
-^
+^http[s]?:\/\/.+ximalaya.+(product/info|/mobile-user/v2/homePage|/vip/v1/recommand/ts|mobile-playpage/playpage/tabs|/mobile-album/album/page/ts|mobile/v1/album/track/ts|product/promotion/v./whole/album/\d+/price/dynamic/ts|business-vip-presale-mobile-web/page/ts).*$ 
+
 [mitm]
+
 hostname = *xima*
+
 */
+
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
