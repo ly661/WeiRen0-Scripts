46bbfc874abc165e682f0f0401cc4bbe6b6470dd
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2022-11-25 09:57:57 +0800

    Create 69luoli++.js

---
+++ 69luoli++.js
@@ -0,0 +1,24 @@
+/*
+脚本功能：69萝莉 会员
+下载地址：https://69luolic.com/?us=283W7F
+在线观看地址: https://h5.l6rwut.com/
+软件版本：所有
+脚本作者：伟人
+作者QQ:55749353
+更新时间：2022-11-25
+问题反馈：QQ+55749353
+作者忠告:注意身体
+QQ会员群：暂无
+TG反馈群：https://t.me/+tOQEwAEP0B81YzVl
+TG频道群：暂无
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+[rewrite_local]
+
+#69萝莉酱
+
+^http[s]?:\/\/h5.+((rwut)|(luoli)|(cmw)).+(com|cn)\/h5\/(movie/detail|search/movie).*$ url script-request-header 69loliJ.js
+
+[mitm]
+hostname = *rwut*,*luoli*,*cmw*
+*/
