146a3c89bbb7faa4a215351e39e4b3389f211778
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2022-12-04 19:49:35 +0800

    Update Milk.js

--- Milk.js
+++ Milk.js
@@ -14,7 +14,8 @@ TG频道群：不用添加
 
 [rewrite_local]
 
-^http[s]?:\/\/ppt.fuaybr.cn\/\/Api\/(getmovexiangqing|getmhepisodesimg).*$ url request-body sign_token=[a-zA-Z0-9]+ request-body sign_token=0a5d55f1d7dfac60f8865c6b1cc58ea6
+^http[s]?:\/\/ppt.fuaybr.cn\/\/Api\/(getmovexiangqing|getmhepisodesimg).*$ url request-body sign_token=[a-zA-Z0-9]+ request-body sign_token=4137a3995468d5b41551f12d7b1e22bd
+
 ^https:\/\/ppt.fuaybr.cn\/\/(Api|Milk)\/(getuserinfo|getstartup|getvideotop) url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/Milk.js
 
 [mitm]
