40670c75824f4e10036ef759dc4937ceaa7c9f3c
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2022-12-06 17:03:31 +0800

    Create Tx.js

---
+++ Tx.js
@@ -0,0 +1,26 @@
+
+/***************************************
+
+脚本功能：醒图 会员
+在线地址：https://txk003.com/line
+软件版本：所有
+脚本作者：伟人
+作者QQ:55749353
+更新时间：2022-12-06
+问题反馈：QQ+55749353
+作者忠告:
+QQ会员群：
+TG反馈群：https://t.me/WeiRenOvO
+TG频道群：https://t.me/WeiRenQAQ
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+**************************************
+
+[rewrite_local]
+
+^http[s]?:\/\/txh\d+.com\/h5\/system\/info url request-body .+ request-body gdC+N8tg08B0aFnzU3dDbbRB/yL9GJ68sHhY7/5reJ42XJ/R8p4rHMPjruhUGY0IImowdQz5tTjdKRetux5DynSu98mfWeaDS1Pnr2DNY+Jy9HpiMXGDxA/NjYDOmW8zEKQ9ZvndGuzLF9XFuA11bg==
+
+
+[mitm]
+
+hostname = txh*
