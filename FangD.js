cb7b7a3465d0f2a5f4dd832ca0e010a44931be35
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2022-12-30 13:50:47 +0800

    Create FangD.js

---
+++ FangD.js
@@ -0,0 +1,49 @@
+/***************************************
+
+
+脚本作者：伟人
+作者QQ:55749353
+更新时间：2022-12-30
+问题反馈：QQ+55749353
+作者忠告: 建议使用在线观看地址观看！
+QQ会员群：PY作者
+TG反馈群：https://t.me/WeiRenOvO
+TG频道群：https://t.me/WeiRenQAQ
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+**************************************
+
+[rewrite_local]
+
+
+^http[s]?:\/\/interface57\.fangdongliqi\.com\/Gold\/GetGoldInfo url script-response-body FangDong.js
+
+[mitm]
+hostname = interface57.fangdongliqi.com
+
+**************************************/
+
+
+var body = $response.body; //声明一个变量body并以响应消息体赋
+let obj = JSON.parse($response.body);
+obj={
+  "Result": "SUCCESS",
+  "GoldInfo": {
+    "Gold": 0,
+    "RoomCount": 0,
+    "ResidualCount": 0,
+    "UseRoomCount": 0,
+    "DaysRemaining": 999,
+    "IsFree": 1,
+    "OverdueDate": "2999-09-09",
+    "OverdueStatus": 0,
+    "FreeDays": 2,
+    "IsBuy": 0,
+    "NowDate": "2022-12-30",
+    "UnpaidTradeId": 0,
+    "ExtendBeginDate": "2022-12-30",
+    "ContractCount": 0,
+    "CanBuy": "Y"
+  }
+}
+$done({body: JSON.stringify(obj)});
