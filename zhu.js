cfe6be3a346e68ae10898fcc36731dc81b933c06
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2023-02-03 11:10:49 +0800

    Update zhu.js

--- zhu.js
+++ zhu.js
@@ -19,7 +19,7 @@ TG频道群：https://t.me/WeiRenQAQ
 
 [mitm]
 
-hostname = 103.41.167.*,www.zhihu.com, api.zhihu.com, zhuanlan.zhihu.com, appcloud2.zhihu.com, m-cloud.zhihu.com,116.136.170.105
+hostname = 120.220.198.*,120.222.234.*,103.41.167.*,www.zhihu.com, api.zhihu.com, zhuanlan.zhihu.com, appcloud2.zhihu.com, m-cloud.zhihu.com,116.136.170.105
 
 [rewrite_local]
 
