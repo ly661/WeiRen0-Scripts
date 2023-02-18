ae9f29b2f781761a340bfec8c864750de26da451
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2023-02-19 01:48:16 +0800

    Update TX1.js

--- TX1.js
+++ TX1.js
@@ -15,16 +15,6 @@ TG频道群：https://t.me/WeiRenQAQ
 
 **************************************
 
-[rewrite_local]
-
-#溏心Vlog
-
-^http[s]?:\/\/tx[a-zA-Z0-9].+(com|cc|cn)\/h5\/system\/info.*$ url script-request-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/TX2.js
-^http[s]?:\/\/tx.*com\/h5\/system\/sendSms url reject-200
-
-
-[mitm]
-hostname = tx*.com
 
 **************************************/
 
