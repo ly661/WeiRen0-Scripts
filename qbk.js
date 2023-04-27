bccc94c957a220e1dfa86e2eebdba6528dc52d65
WeiRen0 <94732914+WeiRen0@users.noreply.github.com>
2023-04-28 00:29:25 +0800

    Update qbk.js

--- qbk.js
+++ qbk.js
@@ -1,7 +1,9 @@
 /*
 [rewrite_local]
 
-^http[s]?:\/\/api\.qbkzhibo\.com\/api\/(member\/login|message\/isBanPM).*$ url response-body "errcode":\d+ response-body "errcode":0
+^http[s]?:\/\/api\.qbkzhibo\.com\/api\/(member\/login|common\/advTextModeration).*$ url response-body "errcode":\d+ response-body "errcode":0
+
+^https://api.qbkzhibo.com/api/message/isBanPM url 307 https://api.qbkzhibo.com/api/common/advTextModeration
 
 [mitm]
 hostname = api.qbkzhibo.com
